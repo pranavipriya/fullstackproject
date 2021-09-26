260a004a2a8462b43d641cf8ea8983c343e408eb
pranavipriya <91413427+pranavipriya@users.noreply.github.com>
2021-09-26 18:33:38 +0530

    Add files via upload

---
+++ MyFirstJavaproject/src/com/fullstackfactory/mfjp/MyJavaClass.java
@@ -0,0 +1,11 @@
+package com.fullstackfactory.mfjp;
+
+public class MyJavaClass {
+
+	public static void main(String[] args){
+		System.out.println("Welcome Full Stack Factory");
+		
+
+	}
+
+}
---
+++ MyFirstJavaproject/src/module-info.java
@@ -0,0 +1,2 @@
+module myFirstJavaproject {
+}
